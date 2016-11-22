130f2ff620332e74c796abb7a28c755dc4490601
ml9951 <ml9951@rit.edu>
2016-11-22 10:56:53 -0500

    Adding test harness

---
+++ static/test/index.js
@@ -0,0 +1,3 @@
+
+
+require('./specs/components/Feed');
---
+++ static/test/specs/components/Feed.jsx
@@ -0,0 +1,15 @@
+// Test Cases for PolicyMenu Component
+
+import React from 'react';
+import {expect} from 'chai';
+import {mount, render} from 'enzyme';
+
+describe('<PolicyMenu/>', () => {
+	it('Mount component', () => {
+		const wrapper = render(
+			<div></div>
+		)	
+	}) 
+	expect(true)
+})
+
