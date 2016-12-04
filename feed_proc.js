3100df23ba5cc8f9f3238d3abaae8e0b91bc10bd
ml9951 <ml9951@rit.edu>
2016-12-04 10:57:35 -0500

    Updating feed_proc to account for new DB schema

--- feed_proc.js
+++ feed_proc.js
@@ -1,7 +1,9 @@
+require('dotenv').config({silent : true})
 var request = require('request-promise');
 var pg = require('pg');
 var util = require('util');
 var fs = require('fs');
+var UntappdClient = require('node-untappd');
 
 function startProc(args){
     var username = args.username;
@@ -9,22 +11,33 @@ function startProc(args){
     var setTimeoutObj = args.setTimeoutObj
     var lastID = args.lastID;
     var waitTime = 16000 //miliseconds
-  
-    var tokens = [access_token];
+
+    var untappd = new UntappdClient();
+
+    untappd.setAccessToken(access_token);
+
+    var tokens = [];
 
     var db = new pg.Client(process.env.OPENSHIFT_POSTGRESQL_DB_URL);
     db.connect();
-  
-    function getFeed(args){
-        var base = 'https://api.untappd.com/v4/thepub/local'
-        var url = `${base}?access_token=${tokens[0]}&lat=${args.lat}&lng=${args.lng}&min_id=${args.min_id}&radius=25`
-        console.log(url)
-        return request(url);
-    }
 
-    function getBeer(bid){
-        var url = `https://api.untappd.com/v4/beer/info/${bid}?access_token=${tokens[0]}`;
-        return request(url);
+    function dbInsert(table, primary_key, values){
+        values = values.map(v => {
+            if(typeof(v) === 'string'){
+                return `$$${v}$$`
+            }else if(typeof(v) === 'object'){
+                return v.val;
+            }
+            return v
+        })
+        return db.query(`
+            INSERT INTO ${table} (${values.join(',')})
+                ON CONFLICT (${primary_key}) DO NOTHING;
+        `, (err, result) => {
+            if(err){
+                console.log(err)
+            }
+        })
     }
 
     function processCheckin(checkin){
@@ -32,41 +45,62 @@ function startProc(args){
             console.log('Skipping due to no url')
             return;
         }
-        getBeer(checkin.beer.bid).then(beer_data => {
-            var beer = undefined;
-            try{
-                beer = JSON.parse(beer_data).response.beer;
-            }catch(e){
-                console.log('JSON.parse error! ' + beer_data)
+
+        untappd.beerInfo((error, beer_data) => {
+            if(error){
+                console.log(error);
                 return;
             }
+            beer = beer_data.response.beer;
             var date = new Date(checkin.created_at);
             var formattedDate = util.format('%d-%d-%d %d:%d:%d', date.getFullYear(), 
                 date.getMonth()+1, date.getDate(), date.getHours(), date.getMinutes(), date.getSeconds());
             if(beer.rating_score >= 4.0){
-                console.log('Inserting beer %s: %s', checkin.brewery.brewery_name, checkin.beer.beer_name)
+                // Insert checkin
+                dbInsert('checkins', 'checkin_id', [
+                    checkin.checkin_id, 
+                    checkin.beer.bid, 
+                    checkin.venue.venue_id,
+                    checkin.brewery.brewery_id,
+                    formattedDate,
+                    username
+                ])
+                // Insert brewery
+                dbInsert('breweries', 'brewery_id', [
+                    checkin.brewery.brewery_id,
+                    checkin.brewery.brewery_name,
+                    checkin.brewery.brewery_slug,
+                    checkin.brewery.contact.twitter,
+                    checkin.brewery.contact.facebook,
+                    checkin.brewery.contact.url
+                ])
+                // Insert beer
+                dbInsert('beers_', 'bid', [
+                    checkin.beer.bid,
+                    checkin.beer.beer_name,
+                    beer.rating_score,
+                    checkin.brewery.brewery_id,
+                    checkin.beer.beer_slug,
+                    checkin.beer.beer_style,
+                    checkin.beer.beer_abv,
+                    checkin.beer.beer_ibu
+                ])
 
-                var q = util.format('INSERT INTO beers (checkin_id, name, brewery, venue, created, rating, lat, lon, username, pic, bid, beer_slug, brewery_id, brewery_slug, venue_id, venue_slug, geom)' + 
-                                    ' SELECT %d, $$%s$$, $$%s$$, $$%s$$, $$%s$$, %d, %d, %d, $$%s$$, $$%s$$, %d, $$%s$$, %d, $$%s$$, %d, $$%s$$, %s WHERE NOT ' + 
-                                    'EXISTS(SELECT 1 FROM beers WHERE checkin_id = %d)',
-                                    checkin.checkin_id, checkin.beer.beer_name, checkin.brewery.brewery_name,
-                                    checkin.venue.venue_name, formattedDate, beer.rating_score, 
-                                    checkin.venue.location.lat, checkin.venue.location.lng, username, 
-                                    checkin.beer.beer_label, checkin.beer.bid, checkin.beer.beer_slug,
-                                    checkin.brewery.brewery_id, checkin.brewery.brewery_slug, 
-                                    checkin.venue.venue_id, checkin.venue.venue_slug, 
-                                    `ST_GeomFromText('POINT(${checkin.venue.location.lng} ${checkin.venue.location.lat})', 4326)`,
-                                    checkin.checkin_id);
-                console.log(q)
-                db.query(q).then(function(){}).catch(function(err){
-                    console.log(err)
-                }); 
-            }else{
-                console.log('Skipping %s, rating = %d', checkin.beer.beer_name, beer.rating_score);
+                dbInsert('venues', 'venue_id', [
+                    checkin.venue.venue_id,
+                    checkin.venue.venue_name,
+                    checkin.venue.venue_slug,
+                    checkin.venue.contact.twitter,
+                    checkin.venue.contact.venue_url,
+                    checkin.venue.primary_category,
+                    checkin.venue.is_verified,
+                    {
+                        val : `ST_GeomFromText('POINT(${checkin.venue.location.lng} ${checkin.venue.location.lat}})', 4326)`
+                    }
+                ])
+
             }
-        }).catch(err => {
-            console.log(err);
-        })
+        }, {BID : checkin.beer.bid})
     }
 
     function iter(){
@@ -75,37 +109,36 @@ function startProc(args){
         try{
             db.query(query).then(result => {
                 var row = result.rows[0];
-                getFeed({'lat' : row.lat, 'lng' : row.lon, 'min_id' : lastID}).then(feedData => {
-                    var data = undefined; 
-                    try{       
-                        data = JSON.parse(feedData);
-                    }catch(e){
-                        console.log(data)
-                        console.log('Error parsing JSON (iter)! ' + data)
-                        setTimeoutObj(setTimeout(iter, waitTime));//try again
+                untappd.pubFeed((err, feedData) => {
+                    if(err){
+                        console.log(err)
+                        console.log('Access %s token exhausted, recycling...', tokens[0])
+                        tokens.push(untappd.getAccessToken())
+                        untappd.setAccessToken(tokens.shift())
+                        setTimeoutObj(setTimeout(iter, waitTime))//try again
+                    }else{
+                        var checkins = feedData.response.checkins.items;
+                        console.log('\n' + checkins.length + ' checkins found')
+              
+                        for(var i = 0; i < checkins.length; i++){
+                            processCheckin(checkins[i]);
+                        }
+                        console.log('len = ' + checkins.length)
+                        console.log('waitTime = ' + waitTime)
+                        if(checkins.length > 12 && lastID != 0){
+                            waitTime = waitTime / 2;
+                        }else if(checkins.length == 0 && waitTime < 1800000){// don't wait longer than 30 minutes
+                            waitTime = waitTime * 2;
+                        }
+                        lastID = checkins.length > 0 ? checkins[0].checkin_id : lastID;
+                        console.log(username + 'Going to sleep for ' + (waitTime/1000) + ' seconds')
+                        setTimeoutObj(setTimeout(iter, waitTime));
                     }
-                    var checkins = data.response.checkins.items;
-                    console.log('\n' + checkins.length + ' checkins found')
-          
-                    for(var i = 0; i < checkins.length; i++){
-                        processCheckin(checkins[i]);
-                    }
-                    console.log('len = ' + checkins.length)
-                    console.log('waitTime = ' + waitTime)
-                    if(checkins.length > 12 && lastID != 0){
-                        waitTime = waitTime / 2;
-                    }else if(checkins.length == 0 && waitTime < 1800000){// don't wait longer than 30 minutes
-                        waitTime = waitTime * 2;
-                    }
-                    lastID = checkins.length > 0 ? checkins[0].checkin_id : lastID;
-                    console.log(username + 'Going to sleep for ' + (waitTime/1000) + ' seconds')
-                    setTimeoutObj(setTimeout(iter, waitTime));
-                }).catch(err => {
-                    console.log(err)
-                    console.log('Access %s token exhausted, recycling...', tokens[0])
-                    tokens.push(tokens.shift());
-                    setTimeoutObj(setTimeout(iter, waitTime))//try again
-                });
+                }, {
+                    lat : row.lat,
+                    lng : row.lon,
+                    min_id : lastID
+                })
             }).catch(function(err){
                 console.log(err)
             })
