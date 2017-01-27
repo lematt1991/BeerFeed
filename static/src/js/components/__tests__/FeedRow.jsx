2def0095a1b944c51c8633577054ff3fd9ee6805
ml9951 <ml9951@rit.edu>
2017-01-26 20:07:34 -0500

    Moving from karma to jest

--- static/src/js/components/__tests__/FeedRow.jsx
+++ static/src/js/components/__tests__/FeedRow.jsx
@@ -1,3 +1,21 @@
 import React from 'react'
+import {shallow} from 'enzyme'
 
 import FeedRow from '../FeedRow'
+import {Checkins1} from '../../test_data/Checkins'
+
+describe('<FeedRow/>', () => {
+
+	it('Mounts', () => {
+		const context = {
+			router : {
+				push : jest.fn()
+			}
+		}
+
+		var wrapper = shallow(<FeedRow {...Checkins1[0]}/>, {context})
+
+
+
+	})
+})
