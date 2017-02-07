5ffe8d8d84137599ef54d4e624276bb58166f0d3
ml9951 <ml9951@rit.edu>
2017-02-06 20:53:02 -0500

    Adding more twitter-bot tests

--- __tests__/twitter-bot.js
+++ __tests__/twitter-bot.js
@@ -6,6 +6,7 @@ describe('twitter-bot', () => {
 	var TwitterBot;
 	var TWITTER_KEY;
 	var TWITTER_SECRET;
+	var toISOStub;
 
 	beforeAll(() => {
 		connectStub = sinon.stub(Client.prototype, 'connect')
@@ -14,17 +15,18 @@ describe('twitter-bot', () => {
 		process.env.TWITTER_KEY = 'twitter_key'
 		TWITTER_SECRET = process.env.TWITTER_SECRET
 		process.env.TWITTER_SECRET = 'twitter_secret'
+		toISOStub = sinon.stub(Date.prototype, 'toISOString', () => 'fake-time')
 	})
 
 	afterAll(() => {
 		connectStub.restore()
 		process.env.TWITTER_SECRET = TWITTER_SECRET;
 		process.env.TWITTER_KEY = TWITTER_KEY;
+		toISOStub.restore()
 	})
 
 	it('Tweets about good beers', done => {
 		var bot = new TwitterBot('username', 'token', 'token_secret')
-		var toISOStub = sinon.stub(Date.prototype, 'toISOString', () => 'fake-time')
 
 		var queryStub = sinon.stub(Client.prototype, 'query', query => new Promise((resolve, reject) => {
 			resolve({
@@ -50,7 +52,64 @@ describe('twitter-bot', () => {
 				query : queryStub.getCall(2).args[0],
 				post : bot.T.post.mock.calls[0][1]
 			}).toMatchSnapshot()
-			toISOStub.restore()
+			queryStub.restore()
+			done()
+		})
+		.catch(err => {
+			console.log(err)
+		})
+	})
+
+	it('Is 140 character aware', done => {
+		var bot = new TwitterBot('username', 'token', 'token_secret')
+
+		var queryStub = sinon.stub(Client.prototype, 'query', query => new Promise((resolve, reject) => {
+			resolve({
+				rows : [{
+					beer : 'Abraxas',
+					bid : 77322,
+					venue_id : 3803461,
+					count : 9,
+					rating : 4.44918012619019,
+					date : '2017-02-04 00:07:28',
+					username : 'nyc_feed',
+					brewery : 'Perennial Artisan Ales',
+					venue : 'As Is NYC',
+					twitter : '@asisnyc'
+				}, 
+				{
+					beer : 'Abraxassssss',
+					bid : 77322,
+					venue_id : 3803461,
+					count : 9,
+					rating : 4.44918012619019,
+					date : '2017-02-04 00:07:28',
+					username : 'nyc_feed',
+					brewery : 'Perennial Artisan Ales',
+					venue : 'As Is NYC',
+					twitter : '@asisnyc'
+				},
+				{
+					beer : 'Abraxasssssssssssssssssssssssssssssssssssssssssssssssssssss',
+					bid : 77322,
+					venue_id : 3803461,
+					count : 9,
+					rating : 4.44918012619019,
+					date : '2017-02-04 00:07:28',
+					username : 'nyc_feed',
+					brewery : 'Perennial Artisan Ales',
+					venue : 'As Is NYC',
+					twitter : '@asisnyc'
+				}]
+			})
+		}))
+
+		bot.T.post = jest.fn(() => new Promise((resolve, reject) => resolve()))
+
+		bot.check().then(() => {
+			expect(bot.T.post.mock.calls[1][1].status.match(/#beer/)).toBeNull()
+			expect(bot.T.post.mock.calls[2][1].status.match('@asisnyc')).toBeNull()
+			queryStub.restore()
 			done()
 		})
 		.catch(err => {
