e5ff1a84bb25b58513bceae3d6ca772e0bf1be97
ml9951 <ml9951@rit.edu>
2017-04-30 13:47:29 -0400

    Fetch users location when app comes into foreground

--- mobile/Init.js
+++ mobile/Init.js
@@ -1,18 +1,40 @@
 import { Location, Constants, Permissions } from 'expo';
 import store from './Store'
 import * as LocationActions from './actions/LocationActions'
+import {AppState} from 'react-native'
 
-//Get the users location
-Permissions.askAsync(Permissions.LOCATION)
-.then(({status}) => {
-	if(status === 'granted'){
-		return Location.getCurrentPositionAsync({})
-	}else{
-		console.log(status)
+function getLocation(){
+	//Get the users location
+	Permissions.askAsync(Permissions.LOCATION)
+	.then(({status}) => {
+		if(status === 'granted'){
+			return Location.getCurrentPositionAsync({})
+		}else{
+			console.log(status)
+		}
+	})
+	.then(location => {
+		if(location){
+			const latitudeDelta = store.getState().location.latitudeDelta || 0.022;
+			const longitudeDelta = store.getState().location.longitudeDelta || 0.121;
+			store.dispatch(LocationActions.setLocation({
+				latitude : location.coords.latitude,
+				longitude : location.coords.longitude,
+				latitudeDelta,
+				longitudeDelta
+			}))
+		}
+	})	
+}
+
+getLocation()
+
+var currentState = AppState.currentState;
+function handleAppStateChange(nextAppState){
+	if(currentState.match(/inactive|background/) && nextAppState === 'active'){
+		//App just came to the foreground
+		getLocation()
 	}
-})
-.then(location => {
-	if(location){
-		store.dispatch(LocationActions.setLocation(location.coords))
-	}
-})	
+	currentState = nextAppState;
+
+}
