b47a09be636a2c320397481c74e8ddb9b05c414d
ml9951 <ml9951@rit.edu>
2017-06-03 11:42:53 -0400

    Load react-native-permissions in MainApplication.java

--- mobile/android/app/src/main/java/com/beerfeed/MainApplication.java
+++ mobile/android/app/src/main/java/com/beerfeed/MainApplication.java
@@ -10,6 +10,8 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
+import com.joshblour.reactnativepermissions.ReactNativePermissionsPackage; // <--- import
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -25,8 +27,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new MapsPackage(),
-            new VectorIconsPackage()
+          new MapsPackage(),
+          new VectorIconsPackage(),
+          new ReactNativePermissionsPackage()
       );
     }
   };
