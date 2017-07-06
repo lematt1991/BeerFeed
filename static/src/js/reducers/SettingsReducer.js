aa007fd88c5abd789d6bdd7de0f57edddd3f9aa4
ml9951 <ml9951@rit.edu>
2017-07-06 16:01:54 -0400

    Updating cookies dep

--- static/src/js/reducers/SettingsReducer.js
+++ static/src/js/reducers/SettingsReducer.js
@@ -1,22 +1,24 @@
-import cookie from 'react-cookie';
+import Cookies from 'universal-cookie'
 import {CHANGE_FEED, CHANGE_CHECKIN_COUNT_THRESHOLD, FETCH_FEEDS} from '../actions/Types'
 import {browserHistory} from 'react-router'
+
+const cookies = new Cookies()
 
 const initialState = {
 	feeds : {},
 	currentFeed : 
-		browserHistory.getCurrentLocation().query.feed ||
-		cookie.load('beerFeedLocation') ||
+		//browserHistory.getCurrentLocation().query.feed ||
+		cookies.get('beerFeedLocation') ||
 		'nyc_feed',
 	checkin_count_threshold : 3
 }
 
-cookie.save('beerFeedLocation', initialState.currentFeed)
+cookies.set('beerFeedLocation', initialState.currentFeed)
 
 export default (state=initialState, action) => {
 	switch(action.type){
 		case CHANGE_FEED:
-			cookie.save('beerFeedLocation', action.payload)
+			cookies.set('beerFeedLocation', action.payload)
 			return {...state, currentFeed : action.payload}
 		case CHANGE_CHECKIN_COUNT_THRESHOLD:
 			return {...state, checkin_count_threshold : action.payload}
