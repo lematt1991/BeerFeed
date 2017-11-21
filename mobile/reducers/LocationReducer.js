c9840cdfeeefad2feca91fc93f4738625ed02c3a
ml9951 <ml9951@rit.edu>
2017-11-21 08:06:02 -0500

    Setting nyc to default location

--- mobile/reducers/LocationReducer.js
+++ mobile/reducers/LocationReducer.js
@@ -1,6 +1,11 @@
 import {SET_LOCATION} from '../actions/Types'
 
-const initialState = {}
+const initialState = {
+	longitude: -73.9479,
+  latitude: 40.789,
+  latitudeDelta : .022,
+  longitudeDelta : .121
+}
 
 export default (state = initialState, action) => {
 	switch(action.type){
